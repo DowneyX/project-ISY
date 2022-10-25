fef1211eb722640d26b8bf683895ca98be5144f6
TKTurbo <t.kreder@st.hanze.nl>
2022-10-25 13:22:26 +0200

    tictactoe-view.fxml en home-view.fxml. kan nog niet navigeren

--- src/main/java/isy/team4/projectisy/MainApplication.java
+++ src/main/java/isy/team4/projectisy/MainApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Home");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/isy/team4/projectisy/controller/HomeController.java
+++ src/main/java/isy/team4/projectisy/controller/HomeController.java
@@ -1,4 +1,46 @@
 package isy.team4.projectisy.controller;
 
+import isy.team4.projectisy.MainApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomeController extends Controller {
+    @FXML
+    private Button tictactoe;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    /**
+     * Simple method to facilitate navigating between scenes
+     * @param actionEvent
+     */
+    @FXML
+    public void navigate(ActionEvent actionEvent) throws IOException {
+        Stage stage;
+        Parent root;
+
+        if(actionEvent.getSource() == tictactoe) {
+            stage = (Stage) tictactoe.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("tictactoe-view.fxml"));
+        } else {
+            return;
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
