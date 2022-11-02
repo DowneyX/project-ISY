30cc1688b48d7ad34c32b8973a7496aba0185869
Mitchellw1233 <mitchellwolters1233@gmail.com>
2022-11-02 18:08:28 +0100

    added board logic to ruleset

--- src/main/java/isy/team4/projectisy/model/rule/IRuleSet.java
+++ src/main/java/isy/team4/projectisy/model/rule/IRuleSet.java
@@ -2,8 +2,11 @@ package isy.team4.projectisy.model.rule;
 
 import isy.team4.projectisy.model.player.IPlayer;
 import isy.team4.projectisy.util.Board;
+import isy.team4.projectisy.util.Vector2D;
 
 public interface IRuleSet {
+    char[] getAllowedInitials();
+    Vector2D getBoardSize();
     void setTurn(Board board, Board newBoard);
     boolean isLegal();
     Board handleBoard(Board board);
