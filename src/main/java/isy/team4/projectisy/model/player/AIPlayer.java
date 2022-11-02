e2ac2565aa8ae4259c691a7ac9bd55619fdbf170
downey_x <dutch.x@outlook.com>
2022-11-02 13:30:05 +0100

    update AI player

--- src/main/java/isy/team4/projectisy/model/player/AIPlayer.java
+++ src/main/java/isy/team4/projectisy/model/player/AIPlayer.java
@@ -24,6 +24,75 @@ public class AIPlayer implements IPlayer {
     @Override
     public Vector2D getMove(Board board) {
         // TODO: AI Move
+        // welcome to if hell
+        if (initial == 'X') {
+            // if there is a winning move play winning move
+            for (int i = 0; i < 3; i++) {
+                // horizontal
+                if (board.getElement(i, 0) == "X" && board.getElement(i, 1) == "X" && board.getElement(i, 2) == null) {
+                    return new Vector2D(i, 2);
+                }
+
+                if (board.getElement(i, 0) == "X" && board.getElement(i, 1) == null && board.getElement(i, 2) == "X") {
+                    return new Vector2D(i, 1);
+                }
+
+                if (board.getElement(i, 0) == null && board.getElement(i, 1) == "X" && board.getElement(i, 2) == "X") {
+                    return new Vector2D(i, 0);
+                }
+
+                // vertical
+                if (board.getElement(0, i) == null && board.getElement(1, i) == "X" && board.getElement(2, i) == "X") {
+                    return new Vector2D(0, i);
+                }
+
+                if (board.getElement(0, i) == "X" && board.getElement(1, i) == null && board.getElement(2, i) == "X") {
+                    return new Vector2D(1, i);
+                }
+
+                if (board.getElement(0, i) == "X" && board.getElement(1, i) == "X" && board.getElement(2, i) == null) {
+                    return new Vector2D(2, i);
+                }
+
+            }
+
+            // diagonal
+            if (board.getElement(0, 0) == null && board.getElement(1, 1) == "X" && board.getElement(2, 2) == "X") {
+                return new Vector2D(0, 0);
+            }
+
+            if (board.getElement(0, 0) == "X" && board.getElement(1, 1) == null && board.getElement(2, 2) == "X") {
+                return new Vector2D(1, 1);
+            }
+
+            if (board.getElement(0, 0) == "X" && board.getElement(1, 1) == "X" && board.getElement(2, 2) == null) {
+                return new Vector2D(2, 2);
+            }
+
+            // anti diagonal
+
+            if (board.getElement(0, 2) == null && board.getElement(1, 1) == "X" && board.getElement(2, 0) == "X") {
+                return new Vector2D(0, 2);
+            }
+
+            if (board.getElement(0, 2) == "X" && board.getElement(1, 1) == null && board.getElement(2, 0) == "X") {
+                return new Vector2D(1, 1);
+            }
+
+            if (board.getElement(0, 2) == "X" && board.getElement(1, 1) == "X" && board.getElement(2, 0) == null) {
+                return new Vector2D(2, 0);
+            }
+
+            // if there is a move that should be blocked block it
+
+            // if board is empty pick bottom left corner
+
+            // if bottom left has X and middle is not filled (we've won)
+
+            // if bottom left has X and middle is filled (probably a tie)
+
+        }
+
         return new Vector2D(0, 0);
     }
 
--- src/main/java/isy/team4/projectisy/util/Board.java
+++ src/main/java/isy/team4/projectisy/util/Board.java
@@ -3,14 +3,14 @@ package isy.team4.projectisy.util;
 import java.util.Arrays;
 
 public class Board {
-    protected int width;  // Could be private if no specific boards are needed
-    protected int height;  // Could be private if no specific boards are needed
-    protected String[][] board;  // Could be private if no specific boards are needed
+    protected int width; // Could be private if no specific boards are needed
+    protected int height; // Could be private if no specific boards are needed
+    protected String[][] board; // Could be private if no specific boards are needed
 
     public Board(int width, int height) {
         this.width = width;
         this.height = height;
-        this.board = new String[height][width];  // reversed for proper x-y notation
+        this.board = new String[height][width]; // reversed for proper x-y notation
     }
 
     public void clear() {
@@ -28,13 +28,21 @@ public class Board {
         this.board[y][x] = value;
     }
 
+    public String getElement(int x, int y) {
+        if (x >= this.width || y >= this.height) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return this.board[y][x];
+    }
+
     /**
      * For debugging purposes
+     * 
      * @return The board array displayed as a string with linebreaks
      */
     public String toString() {
         StringBuilder res = new StringBuilder();
-        for(String[] row : board) {
+        for (String[] row : board) {
             res.append(Arrays.toString(row)).append("\n");
         }
         return res.toString();
