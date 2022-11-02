1e9b9187ca627dced8668c845bba68fe958da2b2
Mitchellw1233 <mitchellwolters1233@gmail.com>
2022-11-03 00:37:02 +0100

    refactor board to have player object

--- src/main/java/isy/team4/projectisy/util/Board.java
+++ src/main/java/isy/team4/projectisy/util/Board.java
@@ -1,31 +1,49 @@
 package isy.team4.projectisy.util;
 
+import isy.team4.projectisy.model.player.IPlayer;
+
 import java.util.Arrays;
 
 public class Board {
     protected int width;  // Could be private if no specific boards are needed
     protected int height;  // Could be private if no specific boards are needed
-    protected String[][] board;  // Could be private if no specific boards are needed
+    protected IPlayer[][] board;  // Could be private if no specific boards are needed
 
     public Board(int width, int height) {
         this.width = width;
         this.height = height;
-        this.board = new String[height][width];  // reversed for proper x-y notation
+        this.board = new IPlayer[height][width];  // reversed for proper x-y notation
+    }
+
+    public IPlayer[][] getData() {
+        return this.board;
     }
 
     public void clear() {
-        this.board = new String[this.height][this.width];
+        this.board = new IPlayer[this.height][this.width];
     }
 
     /**
      * @throws ArrayIndexOutOfBoundsException when x or y out of bounds
      */
-    public void setElement(String value, int x, int y) {
+    public void setElement(IPlayer player, int x, int y) {
         if (x >= this.width || y >= this.height) {
             throw new ArrayIndexOutOfBoundsException();
         }
 
-        this.board[y][x] = value;
+        this.board[y][x] = player;
+    }
+
+    public char[][] toChars() {
+        char[][] chars = new char[this.height][this.width];
+
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.width; x++) {
+                chars[y][x] = this.board[y][x].getInitial();
+            }
+        }
+
+        return chars;
     }
 
     /**
@@ -34,8 +52,8 @@ public class Board {
      */
     public String toString() {
         StringBuilder res = new StringBuilder();
-        for(String[] row : board) {
-            res.append(Arrays.toString(row)).append("\n");
+        for(IPlayer[] row : board) {
+            res.append(Arrays.stream(row).map(IPlayer::getInitial).toString()).append("\n");  // TODO not sure if this works
         }
         return res.toString();
     }
