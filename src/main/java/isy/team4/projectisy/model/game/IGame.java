c495ad07806e10caa65c0376f457f47546079fa9
Mitchellw1233 <mitchellwolters1233@gmail.com>
2022-11-03 19:12:20 +0100

    added getboard method to game

--- src/main/java/isy/team4/projectisy/model/game/IGame.java
+++ src/main/java/isy/team4/projectisy/model/game/IGame.java
@@ -1,11 +1,13 @@
 package isy.team4.projectisy.model.game;
 
 import isy.team4.projectisy.model.player.IPlayer;
+import isy.team4.projectisy.util.Board;
 import isy.team4.projectisy.util.Result;
 
 public interface IGame {
     void start();
     void stop();
+    Board getBoard();
     IPlayer getCurrentPlayer();
     IPlayer[] getPlayers();
     Result getResult();  // Nullable
