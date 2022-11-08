9b8d4f3ddf97dac81d2c02c1c63e44c19c4facb0
Thomas <t.kreder@st.hanze.nl>
2022-11-08 15:48:33 +0100

    Split javafx and cli application for tournament

---
+++ src/main/java/isy/team4/projectisy/CLIApplication.java
@@ -0,0 +1,24 @@
+package isy.team4.projectisy;
+
+import isy.team4.projectisy.server.Server;
+
+public class CLIApplication {
+    public static void main(String[] args) {
+        tournament();
+    }
+
+    /**
+     * Tournament for tic tac toe using the CLI because remotegame is not ready yet.
+     */
+    public static void tournament() {
+        Server server;
+        try {
+            server = new Server("145.33.225.170", 7789);
+        } catch(Exception e) {
+            System.out.println("Kon niet met de server verbinden: " + e);
+            return;
+        }
+
+        server.Requestlogin("ITV2ATEAM4"); // no subscribe needed
+    }
+}
--- src/main/java/isy/team4/projectisy/MainApplication.java
+++ src/main/java/isy/team4/projectisy/MainApplication.java
@@ -1,13 +1,11 @@
 package isy.team4.projectisy;
 
-import isy.team4.projectisy.server.Server;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class MainApplication extends Application {
     @Override
@@ -19,23 +17,7 @@ public class MainApplication extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         launch();
-//        tournament();
-    }
-
-    /**
-     * Tournament for tic tac toe using the CLI because remotegame is not ready yet.
-     */
-    public static void tournament() {
-        Server server;
-        try {
-            server = new Server("ip", 7789);
-        } catch(Exception e) {
-            System.out.println("Kon de server niet starten: " + e);
-            return;
-        }
-
-        server.Requestlogin("name");
     }
 }
