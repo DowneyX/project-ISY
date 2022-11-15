60113e5d07c26fa665f8899912106397414f276b
Thomas <t.kreder@st.hanze.nl>
2022-11-15 01:19:56 +0100

    Can dynamically create gamebuttons and can add styling

--- src/main/java/isy/team4/projectisy/controller/HomeController.java
+++ src/main/java/isy/team4/projectisy/controller/HomeController.java
@@ -1,17 +1,12 @@
 package isy.team4.projectisy.controller;
-
 import isy.team4.projectisy.MainApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Objects;
 
@@ -23,18 +18,22 @@ public class HomeController extends Controller {
 
     public void ticTacToePvAI(ActionEvent actionEvent) {
         p2 = "Local AI";
-        navigateToTicTacToe("tictactoe-view.fxml", "local_pvai");
+        navigate("tictactoe-view.fxml", "local_pvai");
     }
 
     public void ticTacToePvP(ActionEvent actionEvent) {
-        navigateToTicTacToe("tictactoe-view.fxml", "local_pvp");
+        navigate("tictactoe-view.fxml", "local_pvp");
+    }
+
+    public void othelloPvP(ActionEvent actionEvent) {
+        navigate("othello-view.fxml", "local_pvp");
     }
 
     @FXML
-    public void navigateToTicTacToe(String view, String gametype) {
+    public void navigate(String view, String gametype) {
         try {
-            Stage stage = (Stage) dropdown.getScene().getWindow(); // for dropdown using menubutton
-            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(view));
+            Stage stage = (Stage) dropdown.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(view)); // TODO: relative instead of absolute gathering of resources
             Parent root = fxmlLoader.load();
 
             GameController controller = fxmlLoader.getController();
@@ -43,6 +42,7 @@ public class HomeController extends Controller {
             controller.setGameType(gametype);
 
             Scene scene = new Scene(root);
+            scene.getStylesheets().add(Objects.requireNonNull(MainApplication.class.getResource("style.css")).toExternalForm()); // main stylesheet
             stage.setScene(scene);
             stage.show();
         } catch(IOException e) {
--- src/main/java/isy/team4/projectisy/controller/OthelloController.java
+++ src/main/java/isy/team4/projectisy/controller/OthelloController.java
@@ -1,4 +1,27 @@
 package isy.team4.projectisy.controller;
 
-public class OthelloController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class OthelloController extends GameController {
+    @FXML
+    private GridPane grid;
+    @FXML
+    public void initialize() {
+        int wh = 8;
+
+        for(int i = 0; i < wh*wh; i++) {
+            Node button = new Button();
+            button.setId(Integer.toString(i));
+            button.getStyleClass().add("board-button");
+            grid.add(button, i % wh, i / wh);
+        }
+
+    }
+
+    public void startGame(ActionEvent actionEvent) {
+    }
 }
